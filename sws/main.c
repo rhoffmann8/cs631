dda67da00bcd1a5b0f7ffa29559dc3d796ca2d61
Rob Hoffmann <rhoffmann8@gmail.com>
2011-11-13 23:34:15 -0500

    add daemonize, make cur_connections global and decrement in sigchld handler

--- sws/main.c
+++ sws/main.c
@@ -1,6 +1,9 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
 #include <sys/socket.h>
+#include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 
@@ -8,6 +11,7 @@
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <strings.h>
 #include <unistd.h>
 
@@ -17,45 +21,79 @@
 #define MAX_CONN 20
 #define PENDING_CONN 10
 
+/* Default logfile, if none given */
+#define LOGFILE "sws.log"
+
 int main(int, char**);
 void mainloop(void);
 void reap(int);
 void usage(void);
 
 struct swsopts opts;
+int cur_connections, ipv6;
 
 void
 mainloop() {
 
 	struct sockaddr_in sws;
+	struct sockaddr_in6 sws6;
 	struct sigaction sig;
 	pid_t pid;
-	int sock;
-	int conn, cur_connections, max_connections, pending_connections;
-	char buf[1024];
-	char str[INET_ADDRSTRLEN];
 	socklen_t sin_size;
+	int sock, domain;
+	int conn, max_connections, pending_connections;
+	char buf[1024];
 
 	/* Set port */
 	if (opts.port) {
-		if(!(sws.sin_port = htons(opts.port))) {
-			fprintf(stderr, "Invalid port\n");
-			exit(EXIT_FAILURE);
+		if (!ipv6) {
+			if (!(sws.sin_port = htons(opts.port))) {
+				fprintf(stderr, "Invalid port\n");
+				exit(EXIT_FAILURE);
+				/* NOTREACHED */
+			}
+		} else {
+			if (!(sws6.sin6_port = htons(opts.port))) {
+				fprintf(stderr, "Invalid port\n");
+				exit(EXIT_FAILURE);
+				/* NOTREACHED */
+			}
 		}
+	} else {
+		if (!ipv6)
+			sws.sin_port = htons(8080);
+		else
+			sws6.sin6_port = htons(8080);
 	}
+
+	/* Set domain */
+	if (!ipv6)
+		domain = AF_INET;
 	else
-		sws.sin_port = htons(8080);
+		domain = AF_INET6;
 
 	/* Set IP */
-	//TODO: ipv6 check
 	if (opts.ip) {
-		if((sws.sin_addr.s_addr = inet_pton(AF_INET, opts.ip, str)) <= 0) {
-			fprintf(stderr, "Invalid IP\n");
-			exit(EXIT_FAILURE);
+		if (!ipv6) {
+			if ((inet_pton(domain, opts.ip, &sws.sin_addr)) <= 0) {
+				fprintf(stderr, "Invalid IP\n");
+				exit(EXIT_FAILURE);
+				/* NOTREACHED */
+			}
+		} else {
+			if ((inet_pton(domain, opts.ip, &sws6.sin6_addr))
+				<= 0) {
+				fprintf(stderr, "Invalid IP\n");
+				exit(EXIT_FAILURE);
+				/* NOTREACHED */
+			}
 		}
+	} else {
+		if (!ipv6)
+			sws.sin_addr.s_addr = INADDR_ANY;
+		else
+			sws6.sin6_addr = in6addr_any;
 	}
-	else
-		sws.sin_addr.s_addr = INADDR_ANY;
 
 	/* Set maximum connections */
 	if (opts.debug == 1)
@@ -64,16 +102,26 @@ mainloop() {
 		max_connections = MAX_CONN;
 
 	/* Create socket */
-	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+	if ((sock = socket(domain, SOCK_STREAM, 0)) < 0) {
 		perror("opening stream socket");
 		exit(EXIT_FAILURE);
+		/* NOTREACHED */
 	}
 
 	/* Bind to specified address(es) */
-	sws.sin_family = AF_INET;
-	if (bind(sock, (struct sockaddr*)&sws, sizeof(sws))) {
-		perror("binding stream socket");
-		exit(EXIT_FAILURE);
+	sws.sin_family = sws6.sin6_family = domain;
+	if (!ipv6) {
+		if (bind(sock, (struct sockaddr*)&sws, sizeof(sws))) {
+			perror("binding stream socket");
+			exit(EXIT_FAILURE);
+			/* NOTREACHED */
+		}
+	} else {
+		if (bind(sock, (struct sockaddr*)&sws6, sizeof(sws6))) {
+			perror("binding stream socket");
+			exit(EXIT_FAILURE);
+			/* NOTREACHED */
+		}
 	}
 
 	/* Set up signal handler */
@@ -81,8 +129,9 @@ mainloop() {
 	sigemptyset(&sig.sa_mask);
 	sig.sa_flags = SA_RESTART;
 	if (sigaction(SIGCHLD, &sig, NULL) < 0) {
-		perror("sigaction error");
+		perror("sigaction");
 		exit(EXIT_FAILURE);
+		/* NOTREACHED */
 	}
 
 	/* Connection accept loop */
@@ -93,6 +142,7 @@ mainloop() {
 	if (listen(sock, pending_connections) < 0) {
 		perror("listen");
 		exit(EXIT_FAILURE);
+		/* NOTREACHED */
 	}
 
 	/* Accept loop */
@@ -100,25 +150,29 @@ mainloop() {
 		conn = accept(sock, 0, &sin_size);
 		cur_connections++;
 		if (conn == -1)
-			perror("connection accept error");
+			perror("accept");
 		else {
 			if (cur_connections > max_connections)
 				close(conn);
 			else {
-				fprintf(stderr, "connection accepted\n");
 				bzero(buf, sizeof(buf));
 				/* Fork to handle connection */
 				if ((pid = fork()) < 0) {
 					perror("error forking for connection");
 					exit(EXIT_FAILURE);
+					/* NOTREACHED */
 				}
 				else if (pid == 0) {
 					/* Child */
 					close(sock);
-					read(conn, buf, 5);
+					bzero(buf, sizeof(buf));
+
+					/* Pass socket to handler */
 					sws_request(conn);
-					cur_connections--;
-					exit(0);
+
+					close(conn);
+					exit(EXIT_SUCCESS);
+					/* NOTREACHED */
 				}
 				else {
 					/* Parent */
@@ -131,20 +185,22 @@ mainloop() {
 
 void
 reap(int sig) {
-	/* implement SIGCHLD handler here */
+	/* Wait for dead processes in non-blocking mode */
+	cur_connections--;
 	while(waitpid(-1, NULL, WNOHANG) > 0);
 }
 
 int
 main(int argc, char **argv) {
 
+	pid_t pid;
 	char flag;
 	extern char *optarg;
-	extern int optopt;
 
 	while((flag = getopt(argc, argv, "6c:dhi:k:l:p:s:")) != -1) {
 		switch(flag) {
 		case '6':
+			ipv6 = 1;
 			break;
 		case 'c':
 			opts.cgidir = optarg;
@@ -165,6 +221,7 @@ main(int argc, char **argv) {
 			if(!(opts.port = atoi(optarg))) {
 				fprintf(stderr, "Invalid port\n");
 				exit(EXIT_FAILURE);
+				/* NOTREAHCED */
 			}
 			break;
 		case 's':
@@ -185,24 +242,31 @@ main(int argc, char **argv) {
 	} else {
 		fprintf(stderr, "No content directory specified\n");
 		exit(EXIT_FAILURE);
+		/* NOTREACHED */
 	}
 
-	if (opts.key && !opts.secdir) {
-		fprintf(stderr, "Key specified without secure directory\n");
-		exit(EXIT_FAILURE);
-	}
-
-	if (!opts.key && opts.secdir) {
-		fprintf(stderr, "Secure mode specified without key\n");
-		exit(EXIT_FAILURE);
-	}
-
+	/* Option error checking done in sws_init */
 	sws_init(opts);
 
-	/* ... */
-	//error check other arguments
+	if (!opts.debug) {
+		/* Daemonize if -d not set */
+		if ((pid = fork()) < 0) {
+			perror("error forking for daemon");
+			exit(EXIT_FAILURE);
+			/* NOTREACHED */
+		}
 
-	/* daemonize if appropriate */
+		/* Exit the parent */
+		if (pid > 0) {
+			exit(EXIT_SUCCESS);
+			/* NOTREACHED */
+		}
+
+		/* Make sure we don't get output on console */
+		close(STDOUT_FILENO);
+		close(STDIN_FILENO);
+		close(STDERR_FILENO);
+	}
 
 	mainloop();
 
@@ -214,4 +278,5 @@ usage(void) {
 	fprintf(stderr,
 		"usage: sws [-6dh][-c dir][-i address][-l file][-p port][-s dir -k key] dir\n");
 	exit(EXIT_FAILURE);
+	/* NOTREACHED */
 }
